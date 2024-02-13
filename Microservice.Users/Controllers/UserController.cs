93a0bbfc71db9b7b204de7a38e18de23e34982f8
steve <steve.lorent@bstorm.be>
2024-02-13 17:12:48 +0100

    Ajoutez des fichiers projet.

---
+++ Microservice.Users/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Et la ça marche aussi chez les users");
+        }
+    }
+}
